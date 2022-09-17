0cbdc822d7751a72519597c69f4e4903ccbbfb70
uancamilo <uancamilo@gmail.com>
2022-09-17 10:38:26 -0500

    limpieza UI

--- components/meta.js
+++ components/meta.js
@@ -33,8 +33,8 @@ export default function Meta() {
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
-        name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        name="Juan Camilo Serna "
+        content="Desarrollador FrontEnd Juan Camilo"
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
