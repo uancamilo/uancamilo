3c6a402a637677bbd453d1cdba41cc3e2f35dc91
uancamilo <uancamilo@gmail.com>
2022-11-02 16:51:54 -0500

    integracion de pdf y google search console

--- components/meta.js
+++ components/meta.js
@@ -2,39 +2,43 @@ import Head from 'next/head'
 
 export default function Meta() {
   return (
-    <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="Juan Camilo Serna "
-        content="Desarrollador FrontEnd Juan Camilo"
-      />
-    </Head>
-  )
+		<Head>
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/favicon/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon/favicon-16x16.png"
+			/>
+			<link rel="manifest" href="/favicon/site.webmanifest" />
+			<link
+				rel="mask-icon"
+				href="/favicon/safari-pinned-tab.svg"
+				color="#000000"
+			/>
+			<link rel="shortcut icon" href="/favicon/favicon.ico" />
+			<meta name="msapplication-TileColor" content="#000000" />
+			<meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+			<meta name="theme-color" content="#000" />
+			<meta
+				name="google-site-verification"
+				content="SmSLs8hW6F2BOxiuyl2zJ367y1w8jxVj6fM3SBgosZo"
+			/>
+			<link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+			<meta
+				name="Juan Camilo Serna "
+				content="Desarrollador FrontEnd Juan Camilo"
+			/>
+		</Head>
+	);
 }
---
+++ components/perfil.js
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import Container from "../components/container";
+
+export default function Perfil(props) {
+	return (
+		<>
+			<div className="pt-16">
+				<div className="relative overflow-hidden bg-white">
+					<div className="mx-auto max-w-7xl">
+						<div className="relative md:z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+							<svg
+								className="absolute z-10 inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
+								fill="currentColor"
+								viewBox="0 0 100 100"
+								preserveAspectRatio="none"
+								aria-hidden="true"
+							>
+								<polygon fill="" points="0,0 50,0 100,100 0,100" />
+							</svg>
+							<main className="mx-4 max-w-7xl">
+								<div className="text-center lg:pr-5">
+									<h1 className="text-4xl pt-7 font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+										<span className="block">Soy Juan Camilo,</span>{" "}
+										<span className="block text-indigo-600 sm:my-8 lg:my-10v">
+											frontEnd dev <span className="italic"> passionate.</span>
+										</span>
+									</h1>
+									<p className="mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-10">
+										Unir creatividad y usabilidad para generar las mejores
+										experiencias de los usuarios{" "}
+										<span className="italic">− mi reto −.</span>
+									</p>
+									<div className="mt-5 sm:mt-8 sm:flex sm:justify-center">
+										<div className="rounded-md shadow">
+											<button
+												onClick={() => {
+													props.onChange(!props.onChange);
+												}}
+												className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 md:py-4 md:px-10 md:text-lg"
+											>
+												Descargar C.V.
+											</button>
+										</div>
+										<div className="mt-3 sm:mt-0 sm:ml-3">
+											<Link href="/contacto">
+												<a className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-100 px-8 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 md:py-4 md:px-10 md:text-lg">
+													¡Contáctame!
+												</a>
+											</Link>
+										</div>
+									</div>
+								</div>
+							</main>
+						</div>
+					</div>
+					<div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+						<img
+							className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
+							src={props.data.avatar_url}
+							alt="uancamilo"
+						/>
+					</div>
+
+					<p>{props.verPDF}</p>
+				</div>
+				<Container>
+					Cards
+				</Container>
+			</div>
+		</>
+	);
+}
