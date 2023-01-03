866ec7d4fe3123406d81c52598f4f79fe45c1c7d
uancamilo <uancamilo@gmail.com>
2023-01-03 14:40:01 -0500

    cambio additionalSitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,9 +2,10 @@ const siteUrl = "https://uancamilo.vercel.app/";
 
 module.exports = {
 	siteUrl,
+	sitemapBaseFileName: "sitemap",
 	generateRobotsTxt: true,
 	robotsTxtOptions: {
 		policies: [{ userAgent: "*", allow: "/" }],
-		additionalSitemaps: false,
+		additionalSitemaps: [`${siteUrl}sitemap.xml`],
 	},
 };
