2ffe451e56c472c6b0153d09251339c5e84306e3
uancamilo <uancamilo@gmail.com>
2023-01-04 09:59:39 -0500

    sitemap funciona en local prueba en servidor

--- pages/sitemap.xml/index.js
+++ pages/sitemap.xml/index.js
@@ -2,13 +2,36 @@ import { getServerSideSitemap } from "next-sitemap";
 import { getAllPostsWithSlug } from "../../lib/api";
 
 export async function getServerSideProps(context) {
-	//fetch urls
+	//fetch urls desde la api de contenful con graphql
 	const urls = await getAllPostsWithSlug();
+
+	//paginas estaticas ingresadas manualmente
+
+	const principales = [
+		{
+			loc: "https://uancamilo.vercel.app/",
+			lastmod: new Date().toDateString("2023-01-02T00:00.000-05:00"),
+			changefreq: "monthly",
+			priority: "1.0",
+		},
+		{
+			loc: "https://uancamilo.vercel.app/contacto",
+			lastmod: new Date().toDateString("2023-01-02T00:00.000-05:00"),
+			changefreq: "monthly",
+			priority: "0.9",
+		},
+	];
+
 	const fields = urls?.map((url) => ({
 		loc: `https://uancamilo.vercel.app/pagina/${url.slug}`,
+		lastmod: new Date().toDateString(url.date),
+		changefreq: "monthly",
+		priority: "0.8",
 	}));
 
-	return getServerSideSitemap(context, fields);
+	principales.push(...fields);
+
+	return getServerSideSitemap(context, principales);
 }
 
 export default function SiteMap() {}
