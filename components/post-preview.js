a52c07dc5f26912375cc4c4a3a79116bc1504728
uancamilo <uancamilo@gmail.com>
2023-01-11 20:14:44 -0500

    primera entrega del footer

--- components/post-preview.js
+++ components/post-preview.js
@@ -17,7 +17,7 @@ export default function PostPreview({
 			<div className="mb-5 ">
 				<CoverImage title={title} slug={slug} url={coverImage.url} />
 			</div>
-			<div className="px-5 md:min-h-[55%] flex flex-col justify-between">
+			<div className="px-5 md:min-h-[50%] flex flex-col justify-between">
 				<div>
 					<h3 className="text-2xl lg:text-3xl mb-3 leading-snug">
 						<Link href={`/pagina/${slug}`}>
