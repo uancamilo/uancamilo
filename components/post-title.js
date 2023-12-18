7582f6f3191870ea901cafecfa4e30ac23359f9a
uancamilo <uancamilo@gmail.com>
2023-12-18 16:55:17 -0500

    cero

--- components/post-title.js
+++
@@ -1,7 +0,0 @@
-export default function PostTitle({ children }) {
-  return (
-		<h1 className="container mx-auto xl:px-28 lg:px-18  pt-28 text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
-			{children}
-		</h1>
-	);
-}
