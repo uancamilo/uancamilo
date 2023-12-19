192246ab16a62cc5dfd352c6d34824ed219c0c9a
uancamilo <uancamilo@gmail.com>
2023-12-19 14:31:14 -0500

    trece

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+	env: {
+		contenfulSpaceId: "6tvxy6letsti",
+		contentfulAccessKey: "Or0cUC8v5hykZjGmsB-bqeFc_Z-yBOqzRlKppuH5Lr0",
+	},
 	images: {
 		loader: "default",
 		remotePatterns: [
