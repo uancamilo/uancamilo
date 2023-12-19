a0e368469df78f0859e87aa97ae6dd09ca54e0cb
uancamilo <uancamilo@gmail.com>
2023-12-19 14:14:04 -0500

    once

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
 import "../styles/index.css";
 
-function MyApp({ Component, pageProps }) {
-	return (
-		<>
-			<Component {...pageProps} />
-		</>
-	);
+export default function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />;
 }
-
-export default MyApp;
