17ee185be78d384f80f66bb5363b7b094816a99a
uancamilo <uancamilo@gmail.com>
2024-01-09 08:22:36 -0500

    ventiuno

--- pages/perfil.js
+++ pages/perfil.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-export default function Perfil() {
-	return <div>perfil</div>;
+import Perfil from "../components/perfil";
+import PerfilPDF from "../components/perfilPDF";
+
+import Head from "next/head";
+import Layout from "../components/layout";
+
+export default function MostrarPerfil() {
+	const [verWeb, setVerWeb] = useState(true);
+	const [data, setData] = useState(null);
+
+	async function fetchData() {
+		try {
+			const response = await fetch("https://api.github.com/users/uancamilo");
+			setData(await response.json());
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	if (!data) {
+		return "loading...";
+	}
+
+	return (
+		<>
+			<Layout>
+				<Head>
+					<title>Perfil | Juan Camilo Serna</title>
+				</Head>
+				{verWeb ? (
+					<Perfil data={data} onChange={setVerWeb} />
+				) : (
+					<PerfilPDF data={data} onChange={setVerWeb} />
+				)}
+			</Layout>
+		</>
+	);
 }
