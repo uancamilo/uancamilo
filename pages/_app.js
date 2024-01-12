6bf41e3c2414928a563b00ad9a9b71132f673c5f
uancamilo <uancamilo@gmail.com>
2024-01-11 23:47:04 -0500

    ventiseis

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import "../styles/index.css";
+import Script from "next/script";
+import { GTM_ID } from "../lib/gtm";
 
-export default function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','${GTM_ID}');`,
+				}}
+			/>
+			<Component {...pageProps} />
+		</>
+	);
 }
+
+export default MyApp;
