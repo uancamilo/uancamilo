80fc6a2ada9d7f7159b686e3256bb63017103370
uancamilo <uancamilo@gmail.com>
2024-01-11 23:32:22 -0500

    venticinco

---
+++ pages/contacto.js
@@ -0,0 +1,162 @@
+import Head from "next/head";
+import Layout from "../components/layout";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
+export default function Contacto() {
+	const {
+		register,
+		handleSubmit,
+		reset,
+		formState: { errors },
+	} = useForm();
+	async function onSubmitForm(values) {
+		let config = {
+			method: "POST",
+			url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
+			headers: {
+				Accept: "application/json, text/plain, */*",
+				"Content-Type": "application/json",
+			},
+			data: values,
+		};
+		try {
+			const res = await axios(config);
+			if (res.status == 200) {
+				reset();
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+	return (
+		<>
+			<Layout>
+				<Head>
+					<title>Contacto | Juan Camilo Serna</title>
+				</Head>
+				<div className="h-[70rem] md:h-[55rem]">
+					<div className="bg-gradient-to-b from-indigo-700 to-purple-600 h-96 w-full">
+						<div className="w-full flex items-center justify-center py-28">
+							<div className="bg-white shadow rounded py-12 lg:px-28 px-8">
+								<p className="md:text-3xl text-xl font-bold leading-7 text-center text-gray-700">
+									Este es el inicio de algo sorprendente.
+								</p>
+								<p className="md:text-2xl text-xl italic leading-7 text-center text-gray-700 pt-5">
+									Enviarme tus datos será el primer paso.
+								</p>
+								<form onSubmit={handleSubmit(onSubmitForm)}>
+									<div className="md:flex justify-around items-center mt-12 ">
+										<div className="md:w-72 flex flex-col">
+											<label className="text-base font-semibold leading-none text-gray-800">
+												Nombres
+											</label>
+											<input
+												tabIndex={0}
+												arial-label="Ingrese sus nombres"
+												type="name"
+												className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+												placeholder="Ingrese sus nombres"
+												{...register("nombre", {
+													required: "No olvides ingresar el nombre.",
+												})}
+											/>
+											<span className="text-red-400">
+												{errors.nombre?.message}
+											</span>
+										</div>
+										<div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+											<label className="text-base font-semibold leading-none text-gray-800">
+												Correo electrónico
+											</label>
+											<input
+												tabIndex={0}
+												arial-label="Cuál es tu correo electrónico"
+												type="email"
+												className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+												placeholder="¿Cuál es tu correo electrónico?"
+												{...register("email", {
+													required: true,
+													pattern: {
+														value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+														message:
+															"Esta debe ser una dirección de correo electrónico válida.",
+													},
+												})}
+											/>
+										</div>
+									</div>
+									<div className="md:flex justify-around items-center mt-8">
+										<div className="md:w-72 flex flex-col">
+											<label className="text-base font-semibold leading-none text-gray-800">
+												Empresa / compañía
+											</label>
+											<input
+												tabIndex={0}
+												role="input"
+												arial-label="Indicame el nombre de tu compañía"
+												type="empresa"
+												className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 "
+												placeholder="Indicame el nombre de tu compañía"
+												{...register("empresa", {
+													required: "Indicame a cual entidad representas",
+												})}
+											/>
+											<span className="text-red-400">
+												{errors.empresa?.message}
+											</span>
+										</div>
+										<div className="md:w-72 flex flex-col md:ml-6 md:mt-0 mt-4">
+											<label className="text-base font-semibold leading-none text-gray-800">
+												Teléfono
+											</label>
+											<input
+												tabIndex={0}
+												arial-label="Desde que país te comunicas"
+												type="telefono"
+												className="text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100"
+												placeholder="Desde que país te comunicas"
+												{...register("telefono", {
+													required: true,
+												})}
+											/>
+										</div>
+									</div>
+									<div>
+										<div className="w-full flex flex-col mt-8">
+											<label className="text-base font-semibold leading-none text-gray-800">
+												Mensaje
+											</label>
+											<textarea
+												tabIndex={0}
+												aria-label="Envía el mensaje"
+												role="textbox"
+												type="mensaje"
+												className="h-36 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-indigo-700 mt-4 bg-gray-100 border rounded border-gray-200 placeholder-gray-100 resize-none"
+												{...register("mensaje", {
+													required: true,
+												})}
+											/>
+										</div>
+									</div>
+									<p className="text-xs leading-3 text-gray-600 mt-4">
+										Al hacer clic en enviar, acepta nuestros términos de
+										servicio, política de privacidad y cómo usamos los datos que
+										acá se envían.
+									</p>
+									<div className="flex items-center justify-center w-full">
+										<input
+											type="submit"
+											className="mt-9 text-base font-semibold leading-none text-white py-4 px-10 bg-indigo-700 rounded hover:bg-indigo-600 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:outline-none"
+											value="Enviar"
+										/>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Layout>
+		</>
+	);
+}
