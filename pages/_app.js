72d0c01115795974166081d6d2e3a2c3b539e06d
Juan Camilo <uancamilo@gmail.com>
2025-12-10 12:41:44 -0500

    GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -7,6 +8,14 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="google-site-verification" content="SmSLs8hW6F2BOxiuyl2zJ367y1w8jxVj6fM3SBgosZo" />
       </Head>
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KBGT5ZFJ');`}
+      </Script>
       <Component {...pageProps} />
     </>
   );
---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KBGT5ZFJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
