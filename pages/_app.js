e2a18eb7331d2cb7ae898cb9efd50405f873ad75
Juan Camilo <uancamilo@gmail.com>
2025-12-10 12:20:48 -0500

    GSC

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="google-site-verification" content="SmSLs8hW6F2BOxiuyl2zJ367y1w8jxVj6fM3SBgosZo" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
